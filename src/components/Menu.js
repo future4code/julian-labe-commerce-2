a7bc0fc8fab2db5220741fd959d30f59aeb53097
caio mantovani <c.saadmantovani@gmail.com>
2020-04-23 14:21:30 -0300

    adicioandno input

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,27 +1,30 @@
 import React from "react";
+import NumericInput from 'react-numeric-input';
 
 export class MenuLateral extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Filtros</h1>
+    
+  
+    render() {
+      return (
+        <div>
+          <h1>Filtros</h1>
 
-            <div className="valor-minimo">
-                <label>Valor Mínimo:</label><br/>
-                <input type="number" name="valorMax" value=""></input>
-            </div>
+              <div className="valor-minimo">
+                  <label>Valor Mínimo:</label><br/>
+                  <NumericInput type="number" min="0" name="valorMax" value=""></NumericInput>
+              </div>
 
-            <div className="valor-maximo">
-                <label>Valor Máximo:</label><br/>
-                <input type="number" name="valorMin" value=""></input>
-            </div>
+              <div className="valor-maximo">
+                  <label>Valor Máximo:</label><br/>
+                  <NumericInput type="number" min="0" name="valorMin" value=""></NumericInput>
+              </div>
 
-            <div className="buscar-produto">
-                <label>Buscar Produto:</label><br/>
-                <input type="text" name="buscar" value=""></input>
-            </div>
+              <div className="buscar-produto">
+                  <label>Buscar Produto:</label><br/>
+                  <input type="text" name="buscar" value=""></input>
+              </div>
 
-      </div>
-    );
-  }
+        </div>
+      );
+    }
 }
